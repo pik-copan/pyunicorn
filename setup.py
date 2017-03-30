6ac600f4b9800003bb20a55553f182014e32b150
ntfrgl <ntfrgl@gmail.com>
2017-03-30 21:38:19 +0200

    Run test suite w/ & w/o Cython
    - create sdist on Travis-CI
    - run `tox` in env without Cython
    - add PyPI classifiers

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ except ImportError:
     CYTHON = False
 
 extensions = [
-    Extension('*', ['pyunicorn/%s/*.%s' % (pkg, 'pyx' if CYTHON else 'c')],
-              include_dirs=[np.get_include()])
+    Extension(
+        'pyunicorn.%s.numerics' % (pkg),
+        sources=['pyunicorn/%s/numerics.%s' % (pkg, 'pyx' if CYTHON else 'c')],
+        include_dirs=[np.get_include()],
+        extra_compile_args=['-O3', '-std=c99'])
     for pkg in ['core', 'timeseries']]
 
 if CYTHON:
@@ -29,18 +32,29 @@ setup(
     long_description="Advanced statistical analysis and modeling of \
 general and spatially embedded complex networks with applications to \
 multivariate nonlinear time series analysis",
-    keywords='complex networks statistics modeling time series analysis \
-nonlinear climate recurrence plot surrogates spatial model',
+    license='BSD',
     author='Jonathan F. Donges',
     author_email='donges@pik-potsdam.de',
     url='http://www.pik-potsdam.de/~donges/pyunicorn/',
-    platforms=['all'],
+    keywords='complex networks statistics modeling time series analysis \
+nonlinear climate recurrence plot surrogates spatial model',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Science/Research'],
+    provides=['pyunicorn'],
     packages=['pyunicorn', 'pyunicorn.core', 'pyunicorn.climate',
               'pyunicorn.timeseries', 'pyunicorn.funcnet',
               'pyunicorn.utils', 'pyunicorn.utils.progressbar'],
+    scripts=[],
     ext_modules=extensions,
     install_requires=open('requirements.txt', 'r').read().split('\n'),
-    provides=['pyunicorn'],
-    scripts=[],
-    license='BSD',
+    platforms=['all']
 )
