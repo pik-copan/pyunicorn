3626844170ef618a38589bd82eeddb2059fdaf05
harmening <nils.harmening@fu-berlin.de>
2017-05-05 00:23:14 +0200

    fix pep8 style errors

--- setup.py
+++ setup.py
@@ -5,17 +5,24 @@ from setuptools.extension import Extension
 
 import numpy as np
 
+
 class lazy_cythonize(list):
     """evaluates extension list lazyly.
     following pattern is taken from http://tinyurl.com/qb8478q"""
     def __init__(self, callback):
         self._list, self.callback = None, callback
+
     def c_list(self):
-        if self._list is None: self._list = self.callback()
+        if self._list is None:
+            self._list = self.callback()
         return self._list
+
     def __iter__(self):
-        for e in self.c_list(): yield e
+        for e in self.c_list():
+            yield e
+
     def __getitem__(self, ii): return self.c_list()[ii]
+
     def __len__(self): return len(self.c_list())
 
 
@@ -25,11 +32,13 @@ try:
 except ImportError:
     CYTHON = False
 
+
 def extensions():
     exts = [
         Extension(
-            '*',sources=['pyunicorn/%s/*.%s' % (pkg, 'pyx' if CYTHON else 'c'),
-                'pyunicorn/%s/_ext/src_fast_numerics.c' % ('timeseries')],
+            '*',
+            sources=['pyunicorn/%s/*.%s' % (pkg, 'pyx' if CYTHON else 'c'),
+                     'pyunicorn/%s/_ext/src_fast_numerics.c' % ('timeseries')],
             include_dirs=[np.get_include()],
             extra_compile_args=['-O3', '-std=c99'])
         for pkg in ['core', 'timeseries']]
