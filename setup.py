c3471c9357782c3608c57ae4e2eade70034efd15
harmening <nils.harmening@fu-berlin.de>
2017-05-05 21:41:44 +0200

    change settings for extensions in setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ except ImportError:
 exts = [
     Extension(
         '*',
-        sources=['pyunicorn/%s/numerics.%s' % (pkg, 'pyx' if CYTHON else 'c')],
+        sources=['pyunicorn/%s/numerics.%s' % (pkg, 'pyx' if CYTHON else 'c'),
+                 'pyunicorn/timeseries/_ext/src_fast_numerics.c'],
         include_dirs=['.', np.get_include()],
         extra_compile_args=['-O3', '-std=c99'])
     for pkg in ['core', 'timeseries']]
