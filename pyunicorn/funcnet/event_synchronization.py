831b10d5e068ca8a9c528609f4d4184882d01b7a
Jonathan Krönke <kroenke@pik-potsdam.de>
2019-01-10 15:35:26 +0100

    Updated testVectorization of event synchronization to keep track of double counting errors.

--- pyunicorn/funcnet/event_synchronization.py
+++ pyunicorn/funcnet/event_synchronization.py
@@ -95,7 +95,7 @@ class EventSynchronization:
     #
 
     @cached_const('base', 'directedES')
-    def directedES(self, legacy=False):
+    def directedES(self):
         """
         Returns the NxN matrix of the directed event synchronization measure.
         The entry [i, j] denotes the directed event synchronization from
@@ -107,8 +107,7 @@ class EventSynchronization:
         for i in range(0, self.__N):
             for j in range(i+1, self.__N):
                 res[i, j], res[j, i] = self._EventSync(eventmatrix[:, i],
-                                                       eventmatrix[:, j],
-                                                       legacy)
+                                                       eventmatrix[:, j])
         return res
 
     def symmetricES(self):
@@ -128,72 +127,7 @@ class EventSynchronization:
         directed = self.directedES()
         return directed - directed.T
 
-    def ECA(self, EventSeriesX, EventSeriesY, delT, tau=0, ts1=None, ts2=None):
-        """
-        Event coincidence analysis:
-        Returns the precursor and trigger coincidence rates of two event series
-        X and Y.
-
-        :type EventSeriesX: 1D Numpy array
-        :arg EventSeriesX: Event series containing '0's and '1's
-        :type EventSeriesY: 1D Numpy array
-        :arg EventSeriesY: Event series containing '0's and '1's
-        :arg delT: coincidence interval width
-        :arg int tau: lag parameter
-        :rtype: list
-        :return: [Precursor coincidence rate XY, Trigger coincidence rate XY,
-                  Precursor coincidence rate YX, Trigger coincidence rate YX]
-        """
-
-        N = self.__N
-        # Count events that cannot be coincided due to tau and delT
-        if not (tau == 0 and delT == 0):
-            # Start of EventSeriesX
-            n11 = np.count_nonzero(EventSeriesX[:tau+delT])
-            # End of EventSeriesX
-            n12 = np.count_nonzero(EventSeriesX[-(tau+delT):])
-            # Start of EventSeriesY
-            n21 = np.count_nonzero(EventSeriesY[:tau+delT])
-            # End of EventSeriesY
-            n22 = np.count_nonzero(EventSeriesY[-(tau+delT):])
-        else:
-            # Instantaneous coincidence
-            n11, n12, n21, n22 = 0, 0, 0, 0
-        # Get time indices
-        if ts1 is None:
-            e1 = np.where(EventSeriesX)[0]
-        else:
-            e1 = ts1[EventSeriesX]
-        if ts2 is None:
-            e2 = np.where(EventSeriesY)[0]
-        else:
-            e2 = ts2[EventSeriesY]
-        del EventSeriesX, EventSeriesY, ts1, ts2
-        # Number of events
-        l1 = len(e1)
-        l2 = len(e2)
-        # Array of all interevent distances
-        dst = (np.array([e1]*l2).T - np.array([e2]*l1))
-        # Count coincidences with array slicing
-        prec12 = np.count_nonzero(np.any(((dst - tau >= 0)
-                                          * (dst - tau <= delT))[n11:, :],
-                                         axis=1))
-        trig12 = np.count_nonzero(np.any(((dst - tau >= 0)
-                                          * (dst - tau <= delT))
-                                         [:, :dst.shape[1]-n22],
-                                         axis=0))
-        prec21 = np.count_nonzero(np.any(((-dst - tau >= 0)
-                                          * (-dst - tau <= delT))[:, n21:],
-                                         axis=0))
-        trig21 = np.count_nonzero(np.any(((-dst - tau >= 0)
-                                          * (-dst - tau <= delT))
-                                         [:dst.shape[0]-n12, :],
-                                         axis=1))
-        # Normalisation and output
-        return (np.float32(prec12)/(l1-n11), np.float32(trig12)/(l2-n22),
-                np.float32(prec21)/(l2-n21), np.float32(trig21)/(l1-n12))
-
-    def _EventSync(self, EventSeriesX, EventSeriesY, legacy=False):
+    def _EventSync(self, EventSeriesX, EventSeriesY):
         """
         Calculates the directed event synchronization from two event series X
         and Y.
@@ -204,7 +138,6 @@ class EventSynchronization:
         :arg EventSeriesY: Event series containing '0's and '1's
         :rtype: list
         :return: [Event synchronization XY, Event synchronization YX]
-
         """
 
         # Get time indices (type boolean or simple '0's and '1's)
@@ -229,27 +162,25 @@ class EventSynchronization:
                           np.repeat(diffymin, lx-2, axis=0))
         tau2 = np.minimum(tau2, 2 * self.taumax)
         # Count equal time events and synchronised events
-        eqtime = 0.5 * (dstxy2.size - np.count_nonzero(dstxy2))
+        eqtime = dstxy2.size - np.count_nonzero(dstxy2)
+
         # Calculate boolean matrices of coincidences
         Axy = (dstxy2 > 0) * (dstxy2 <= tau2)
         Ayx = (dstxy2 < 0) * (dstxy2 >= -tau2)
 
+        # Loop over coincidences and determine number of double counts
+        # by checking at least one event of the pair is also coincided
+        # in other direction
+        countxydouble = countyxdouble = 0
+
+        for i, j in np.transpose(np.where(Axy)):
+            countxydouble += np.any(Ayx[i, :]) or np.any(Ayx[:, j])
+        for i, j in np.transpose(np.where(Ayx)):
+            countyxdouble += np.any(Axy[i, :]) or np.any(Axy[:, j])
+
         # Calculate counting quantities and subtract half of double countings
-        countxy = np.sum(Axy) + eqtime
-        countyx = np.sum(Ayx) + eqtime
-
-        if not legacy:
-            countxydouble = countyxdouble = 0
-            # Loop over coincidences and determine number of double counts
-            # by checking at least one event of the pair is also coincided
-            # in other direction
-            for i, j in np.transpose(np.where(Axy)):
-                countxydouble += np.any(Ayx[i, :]) or np.any(Ayx[:, j])
-            for i, j in np.transpose(np.where(Ayx)):
-                countyxdouble += np.any(Axy[i, :]) or np.any(Axy[:, j])
-
-            countxy = countxy - 0.5 * countxydouble
-            countyx = countyx - 0.5 * countyxdouble
+        countxy = np.sum(Axy) + 0.5 * eqtime - 0.5 * countxydouble
+        countyx = np.sum(Ayx) + 0.5 * eqtime - 0.5 * countyxdouble
 
         norm = np.sqrt((lx-2) * (ly-2))
         return countxy / norm, countyx / norm
