e9637af6d03fb78f2aa11fdf1982224fc660fef5
ntfrgl <ntfrgl@users.noreply.github.com>
2019-01-19 19:44:41 -0800

    Update legacy.py

--- pyunicorn/legacy.py
+++ pyunicorn/legacy.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def new_numpy_version():
-    return '1.15' in np.__version__ or '1.14' in np.__version__
+    return np.__version__ >= '1.14'
 
 
 def set_legacy_mode():
     args = {}
-    if new_numpy_version() is True:
+    if new_numpy_version():
         args['legacy'] = '1.13'
     np.set_printoptions(**args)
