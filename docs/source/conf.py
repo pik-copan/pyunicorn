f567a8c62402f59481db53f7a692d19008306c19
fkuehlein <fritz.kuehlein@student.uni-halle.de>
2024-05-19 14:09:38 +0200

    Revert "DOC/MAINT: suppress cache warnings in `conf.py`"
    This reverts commit 17c81529b6f383d049272051e91c7a5babf9d405.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = False
 modindex_common_prefix = ['pyunicorn.']
 
 # Suppress warnings for test suite.
-suppress_warnings = ['image.nonlocal_uri', 'config.cache']
+suppress_warnings = ['image.nonlocal_uri']
 
 
 # -- Options for HTML output ----------------------------------------------
